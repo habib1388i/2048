a6e800edbed98fdbc9650a8c220c2ffb412cb7e8
Muhammad hidayatullah <muhammadhabibhidayatullah@macbook-pro-muhammad.local>
2018-10-16 20:42:02 +0700

    Initial Commit

---
+++ 2048/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  2048
+//
+//  Created by Muhammad Habib hidayatullah on 16/10/18.
+//  Copyright © 2018 habdev. All rights reserved.
+//
+
+import UIKit
+import WebKit
+class ViewController: UIViewController {
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+            super.viewDidLoad()
+        let url = "https://gabrielecirulli.github.io/2048/"
+            webView.load(URLRequest(url: URL(string: url)!))
+            // Do any additional setup after loading the view, typically from a nib.
+        }
+        
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+        
+        
+}
+
